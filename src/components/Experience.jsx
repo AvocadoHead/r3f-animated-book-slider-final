6965a2bd7a9ab07a1e1f12682f83df75764f9a81
AvocadoHead <55915461+AvocadoHead@users.noreply.github.com>
2026-01-10 13:33:06 +0200

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,27 +1,30 @@
 import { Environment, Float, OrbitControls } from "@react-three/drei";
-// CORRECT IMPORT: Book is in the same folder as Experience
 import { Book } from "./Book"; 
 
 export const Experience = () => {
   return (
     <>
       <Float
-        rotation-x={-0.1}
-        floatIntensity={0.2}
+        rotation-x={0} // upright, no forward lean by default
+        rotation-y={0} 
+        rotation-z={0}
+        floatIntensity={0.4} // gentle bobbing up/down
         speed={2}
-        rotationIntensity={0.5}
+        rotationIntensity={0.2} // very slight rotation
+        floatingRange={[-0.1, 0.1]} // keep it centered
       >
         <Book />
       </Float>
       <OrbitControls 
-        maxPolarAngle={Math.PI / 2} 
+        maxPolarAngle={Math.PI / 2} // Don't go below floor
+        minPolarAngle={0} // Allow looking from top
         minDistance={3}
         maxDistance={15}
       />
       <Environment preset="studio"></Environment>
       <directionalLight
-        position={[2, 5, 2]}
-        intensity={2.5}
+        position={[0, 5, 2]} // Centered light to avoid uneven side lighting
+        intensity={2.0}
         castShadow
         shadow-mapSize-width={2048}
         shadow-mapSize-height={2048}
