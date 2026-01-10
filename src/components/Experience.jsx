dbe0ba3567ae41c24fdd5a921e4da563b41f47c8
AvocadoHead <55915461+AvocadoHead@users.noreply.github.com>
2026-01-10 14:21:23 +0200

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,27 +1,25 @@
 import { Environment, Float, OrbitControls } from "@react-three/drei";
-// CORRECT IMPORT: Sibling file in 'src/components/'
 import { Book } from "./Book"; 
 
 export const Experience = () => {
   return (
     <>
       <Float
-        rotation-x={-Math.PI / 4} // Initial tilt for reading angle
-        floatIntensity={0.5}      // Weighted (0.5 is heavier than 1)
-        speed={2}                 // Speed of the floating bob
-        rotationIntensity={1}     // 1 allows tilting forward/back/side (0.2 was too stiff)
+        rotation-x={-Math.PI / 4} // Initial tilt
+        floatIntensity={0.5}      // Weighted feel
+        speed={2}                 
+        rotationIntensity={1}     // Allows tilting
         floatingRange={[-0.1, 0.1]}
       >
         <Book />
       </Float>
       <OrbitControls 
-        maxPolarAngle={Math.PI / 2} // Don't go under the floor
+        maxPolarAngle={Math.PI / 2} 
         minDistance={3}
         maxDistance={15}
       />
       <Environment preset="studio"></Environment>
       
-      {/* Lighting Fix: Balanced to stop right-page washout */}
       <ambientLight intensity={1.5} />
       <directionalLight
         position={[2, 5, 2]}
