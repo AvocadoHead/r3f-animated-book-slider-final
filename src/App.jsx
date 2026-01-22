39bf67b1121a8d2ad5a33ab7ad2111b638cb4b33
AvocadoHead <55915461+AvocadoHead@users.noreply.github.com>
2026-01-22 09:22:10 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -33,8 +33,8 @@ const getRouteInfo = () => {
 
   // Default home
   return { route: 'home' };
-};
-};
+}; 
+// ^^^ The extra }; was here. It is now gone.
 
 function App() {
   const [loading, setLoading] = useState(true);
